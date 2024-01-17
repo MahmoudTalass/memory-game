40e63687b707525bb1ae9bac37e76bd5cefeea76
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-17 14:06:04 -0500

    Fetch cards info from API when component is mounted

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,1 +1,34 @@
-export default function Game() {}
+import { useState, useEffect } from "react";
+
+export default function Game() {
+   const [score, setScore] = useState(0);
+   const [highestScore, setHighestScore] = useState(0);
+   const [characters, setCharacters] = useState([]);
+   const [isLoading, setIsLoading] = useState(true);
+
+   async function fetchCharacters() {
+      const characters = await fetch(
+         "https://api.attackontitanapi.com/characters/1,2,3,4,5,12,86,87,88,124,184,188"
+      );
+
+      if (!characters.ok) {
+         return;
+      }
+
+      const charactersJson = await characters.json();
+      const charactersSimplified = charactersJson.map((char) => {
+         return {
+            id: char.id,
+            name: char.name,
+            img: char.img,
+         };
+      });
+
+      return charactersSimplified;
+   }
+
+   useEffect(() => {
+      setCharacters(fetchCharacters());
+      setIsLoading(false);
+   }, []);
+}

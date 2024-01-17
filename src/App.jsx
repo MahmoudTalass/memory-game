5175f965a2c8a95002a5fa885f9a298df7b97dc0
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-17 12:40:44 -0500

    Add a main, footer and header to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-export default function App() {}
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default function App() {
+   return (
+      <>
+         <Header />
+         <main></main>
+         <Footer />
+      </>
+   );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+   return (
+      <footer>
+         <p>By Mahmoud T</p>
+      </footer>
+   );
+}

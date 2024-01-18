bf7025d16deae21542bcf164a2da62e104e8eeb8
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-17 20:47:41 -0500

    Import the Game component into the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Game from "./components/Game";
 
 export default function App() {
    return (
       <>
          <Header />
-         <main></main>
+         <main>
+            <Game />
+         </main>
          <Footer />
       </>
    );

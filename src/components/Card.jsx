95c4cba848cd74324540d8d502e8a691db14843c
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-17 21:35:56 -0500

    Remove the current state

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,6 @@
 import { useState } from "react";
 
 export default function Card({ name, img }) {
-   const [clicks, setClicks] = useState(0);
-
    return (
       <div className="card">
          <img src={img} alt={name} className="card-img" />

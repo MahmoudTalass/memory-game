442deb8f30c4a872129bfd72838fd491d1c23a2f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-01-18 15:46:51 -0500

    Import style sheet for lost component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/App.css";
 import "./styles/Game.css";
+import "./styles/Lost.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
